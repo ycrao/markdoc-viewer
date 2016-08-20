8e5fbab1ea1ea154d30bb6860c4b977ce3b4ece3
raoyc <raoyc2009@gmail.com>
2016-08-20 17:07:40 +0800

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,83 @@
 # MarkdocViewer
 
 > MarkdocViewer : reading markdown documentation from open git repository (just in one page) !
-> MarkdocViewer ：从公开的 `git` 仓库阅读 `markdown` 文档 （仅凭一个页面）！
+
+> MarkdocViewer ：从公开的 `git` 仓库阅读 `markdown` 文档 （就一个页面）！
+
+
+## 参数配置
+
+MarkdocViewer默认配置如下：
+
+```javascript
+    this.defaults = {
+            'base_url': 'https://raw.githubusercontent.com/',  //git 公共仓库 raw 基地址
+            'base_dir': '',  //文档所在子目录，默认为空
+            'repo_name': 'yascmf/docs',  //文档仓库名，默认为作者的 YASCMF 文档库
+            'branch_name': 'master',  //文档所在分支，默认为master
+            'index_file': 'index.md',  //目录索引文件，默认为index.md
+            'home_file': 'readme.md',  //目录默认主页文件，默认为readme.md
+            'sider_id': 'sider-menu',  //导航目录所在div的id名
+            'content_id': 'content',  //markdown正文所在div的id名
+        };
+```
+
+以上某些配置，可以通过传入查询串，予以重载新配置：
+
+`https://raoyc.com/markdoc-viewer?dir=&repo=yascmf/docs&branch=master&index=index.md&home=readme.md`
+
+其中，`dir` 查询串对应 `base_dir` 、`repo` 对应 `repo_name` 配置，后面依次类推。请保证，查询串值与仓库文档实际结构目录一致。
+
+```javascript
+    var dir = config('dir', ''),
+        repo = config('repo', 'yascmf/docs'),
+        branch = config('branch', 'master'),
+        index = config('index', 'index.md'),
+        home = config('home', 'readme.md');
+    var config = {
+        //default git raw base url : https://raw.githubusercontent.com/
+        'base_url': 'https://raw.githubusercontent.com/',
+        'base_dir': dir,
+        'repo_name': repo,
+        //default branch name : master
+        'branch_name': branch,
+        'index_file': index,
+        'home_file': home,
+        //sider menu div id
+        'sider_id': 'sider-menu',
+        //markdown content div id
+        'content_id': 'content',
+    };
+    console.log(config);
+    var mv = new MarkdocViewer(config);
+    mv.viewer();
+```
+
+## 在线文档阅读示例
+
+*   [`YASCMF` 文档](https://github.com/yascmf/docs)：
+
+https://raoyc.com/markdoc-viewer?doc=readme.md&dir=&index=index.md&repo=yascmf/docs&home=readme.md
+
+
+*   [`GitBookIO` 文档](https://github.com/GitbookIO/gitbook/docs)：
+
+https://raoyc.com/markdoc-viewer?doc=README.md&dir=docs&index=SUMMARY.md&repo=GitbookIO/gitbook&home=README.md
+
+*   `Overtrue` 写的 [`building-web-apps-with-laravel5` 文档](https://github.com/overtrue/building-web-apps-with-laravel5)：
+
+https://raoyc.com/markdoc-viewer?doc=README.md&dir=&index=SUMMARY.md&repo=overtrue/building-web-apps-with-laravel5&home=README.md
+
+## 鸣谢
+
+本源码使用到以下开源组件：
+
+- [`marked`](https://github.com/chjj/marked)
+
+- [`github-markdown-css`](https://github.com/sindresorhus/github-markdown-css)
+
+- [`highlight.js`](https://highlightjs.org/)
+
+- [`bootstrap`](http://getbootstrap.com/)
+
+- 感谢 [GitHub](https://github.com) 提供项目文档托管服务。
---
+++ js/markdoc_viewer.js
@@ -0,0 +1,159 @@
+    if (!window.localStorage) {
+      window.localStorage = {
+        getItem: function (sKey) {
+          if (!sKey || !this.hasOwnProperty(sKey)) { return null; }
+          return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
+        },
+        key: function (nKeyId) {
+          return unescape(document.cookie.replace(/\s*\=(?:.(?!;))*$/, "").split(/\s*\=(?:[^;](?!;))*[^;]?;\s*/)[nKeyId]);
+        },
+        setItem: function (sKey, sValue) {
+          if(!sKey) { return; }
+          document.cookie = escape(sKey) + "=" + escape(sValue) + "; expires=Tue, 19 Jan 2038 03:14:07 GMT; path=/";
+          this.length = document.cookie.match(/\=/g).length;
+        },
+        length: 0,
+        removeItem: function (sKey) {
+          if (!sKey || !this.hasOwnProperty(sKey)) { return; }
+          document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+          this.length--;
+        },
+        hasOwnProperty: function (sKey) {
+          return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+        }
+      };
+      window.localStorage.length = (document.cookie.match(/\=/g) || window.localStorage).length;
+    }
+
+
+    function merge(obj) {
+      var i = 1
+        , target
+        , key;
+
+      for (; i < arguments.length; i++) {
+        target = arguments[i];
+        for (key in target) {
+          if (Object.prototype.hasOwnProperty.call(target, key)) {
+            obj[key] = target[key];
+          }
+        }
+      }
+
+      return obj;
+    }
+
+    function request(key, val = null) {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            if (pair[0] == key) {
+                return pair[1];
+            }
+        }
+        if ((typeof val == 'string') && (val.constructor == String)) {
+            return (val === '') ? (false) : val;
+        } else {
+            return (false);
+        }
+    };
+
+    function config(name, def = null) {
+        if (request(name) === false) {
+            var req_name = localStorage.getItem(name) ? localStorage.getItem(name) : def;
+        } else {
+            localStorage.setItem(name, request(name));
+            var req_name = request(name);
+        }
+        return req_name;
+    }
+
+    var MarkdocViewer = function(opt) {
+        this.defaults = {
+            'base_url': 'https://raw.githubusercontent.com/',
+            'base_dir': '',
+            'repo_name': 'yascmf/docs',
+            'branch_name': 'master',
+            'index_file': 'index.md',
+            'home_file': 'readme.md',
+            'sider_id': 'sider-menu',
+            'content_id': 'content',
+        };
+        this.options = merge(this.defaults, opt);
+    };
+
+    MarkdocViewer.prototype.marked = function (renderer = new marked.Renderer()) {
+        renderer.table = function (header, body) {
+            return '<table class="table table-bordered table-striped">\n'
+                    + '<thead>\n'
+                    + header
+                    + '</thead>\n'
+                    + '<tbody>\n'
+                    + body
+                    + '</tbody>\n'
+                    + '</table>\n';
+        }
+        marked.setOptions({
+            renderer: renderer,
+            gfm: true,
+            tables: true,
+            breaks: false,
+            pedantic: false,
+            sanitize: true,
+            smartLists: true,
+            smartypants: false,
+            langPrefix: 'language-',
+            //这里使用了highlight对代码进行高亮显示
+
+            highlight: function (code) {
+                return hljs.highlightAuto(code).value;
+            }
+        });
+        return marked;
+    };
+
+    MarkdocViewer.prototype.http_get = function (doc, success, failed) {
+        var xmlHttp = null;
+        var base_dir = (this.options['base_dir'] === '') ? '' : (this.options['base_dir'] + '/');
+        var _base = this.options['base_url'] + this.options['repo_name'] + '/' + this.options['branch_name'] + '/' + base_dir;
+        var _url = _base + doc;
+        if (window.XMLHttpRequest) {
+            xmlHttp = new XMLHttpRequest();  //FireFox、Opera等浏览器支持的创建方式
+        } else {
+            xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");  //IE浏览器支持的创建方式 
+        }
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                success(xmlHttp.responseText);
+            } else {
+                if(failed){
+                    failed(xmlHttp.status);
+                }
+            }
+        }
+        xmlHttp.open("GET", _url, true);
+        xmlHttp.send(null);
+    }
+
+    MarkdocViewer.prototype.viewer = function () {
+        var content = this.options['content_id'];
+        var sider = this.options['sider_id'];
+        var marked = this.marked();
+        var index = this.options['index_file'];
+        var doc = request('doc');
+        if (!doc) {
+            doc = this.options['home_file'];
+        }
+        this.http_get(index, function (menu) {
+            var menu = marked(menu);
+            var pattern = new RegExp('href="([\\w_\\-\\/\\#\\.]+)"', 'gi');
+            menu = menu.replace(pattern, 'href="?doc=$1"');
+            //menu = menu.replace(/href="(\w+)\ + ext + .md"/gi, 'href="?doc=$1.md"');
+            document.getElementById(sider).innerHTML = menu;
+        });
+
+        this.http_get(doc, function (res) {
+            document.getElementById(content).innerHTML = marked(res);
+        });
+    };
